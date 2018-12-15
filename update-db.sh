2fa7c123356c07508ffe1926324539e9add86018
Roman Nix <rnix@yandex.ru>
2018-12-15 13:24:10 +0700

    Uncomment download line

--- update-db.sh
+++ update-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#curl -L http://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2 | bzip2 -d > /tmp/list_of_expired_passports.csv
+curl -L http://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2 | bzip2 -d > /tmp/list_of_expired_passports.csv
 PID=$(pidof -s invalid-passports-go)
 if [ ! -z "${PID}" ]; then
   kill -SIGUSR1 "${PID}"
